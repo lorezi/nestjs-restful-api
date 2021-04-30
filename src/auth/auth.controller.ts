0b089c9b141e972874128aea9aa382773fa1aea6
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-04-30 17:36:03 +0100

    hashed password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,23 @@
 import { UserService } from './../user/user.service';
 import { Body, Controller, Post } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Controller()
 export class AuthController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   @Post('register')
   async register(@Body() body) {
-    return this.userService.createUser(body);
+    const hashedPassword = await bcrypt.hash(body.password, 12);
+    return this.userService.createUser({
+      first_name: body.first_name,
+      last_name: body.last_name,
+      email: body.email,
+      password: hashedPassword,
+    });
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,16 @@
 import { UserModule } from './../user/user.module';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
