16f56df2617dd21a22f4e848453b76f36eb34812
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-04-30 15:54:47 +0100

    added user entity to user controller import

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,10 @@
+import { User } from './models/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
 })
 export class UserModule {}
