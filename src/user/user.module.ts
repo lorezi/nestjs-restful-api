742ea9bce56fffd11f39639086cf086669ee76da
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-04-30 15:57:47 +0100

    generated user service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,11 @@ import { User } from './models/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
+  providers: [UserService],
 })
 export class UserModule {}
