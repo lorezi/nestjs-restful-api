736c66b21da7bd1b1f75b7cd63fc5a3647fd80f3
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-04-30 16:37:23 +0100

    create user added to user service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
+import { UserService } from './../user/user.service';
 import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller()
 export class AuthController {
+  constructor(private userService: UserService) {}
+
   @Post('register')
   async register(@Body() body) {
-    return body;
+    return this.userService.createUser(body);
   }
 }
