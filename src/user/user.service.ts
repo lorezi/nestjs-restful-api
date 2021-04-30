cfa1f0b0561bcc9c3ccb9e5b29f47ba2771c3bc8
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-04-30 16:03:48 +0100

    get users function add to user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './models/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  /**
+   *
+   * @returns list of users
+   */
+  async getUsers(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+}
