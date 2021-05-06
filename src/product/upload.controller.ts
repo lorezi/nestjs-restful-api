b6bc048ef8e584a6c2e5b293b39cd489ea7e5f05
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-05-06 11:50:20 +0100

    file upload validation

--- src/product/upload.controller.ts
+++ src/product/upload.controller.ts
@@ -1,17 +1,47 @@
 import {
+  BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { Express } from 'express';
+import { Express, Request, Response } from 'express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller()
 export class UploadController {
   @Post('upload')
-  @UseInterceptors(FileInterceptor('image'))
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename(req: Request, file: Express.Multer.File, cb) {
+          if (file.mimetype.startsWith('image')) {
+            const randomName = `user-${file.originalname}-${Date.now()}`;
+            return cb(null, `${randomName}${extname(file.originalname)}`);
+          }
+          return cb(new BadRequestException('invalid file upload'), '');
+        },
+      }),
+    }),
+  )
   uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+    if (file.mimetype.startsWith('image')) {
+      return {
+        url: `http://localhost:8000/api/${file.path}`,
+      };
+    }
+
+    throw new BadRequestException('invalid file upload');
+  }
+
+  @Get('uploads/:path')
+  async getImage(@Param('path') path, @Res() res: Response) {
+    res.sendFile(path, { root: 'uploads' });
   }
 }
